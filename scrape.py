704595ae518bfcf1240ed7cb0c45a298b0218b9f
Rohan Bk <rohan7study@gmail.com>
2025-06-16 11:38:16 +0545

    Add git commands

--- scrape.py
+++ scrape.py
@@ -48,3 +48,15 @@ def scrape_book(url):
         
                 
 scrape_book(url)
+
+
+# go to git bash
+# git config --global user.name "Rohan Bk"
+# git config --global user.email "rohan7study@gmail.com"
+
+# git init -> initalize git
+# git status -> if u want to check what are the status of the file
+# git diff -> if u want to check what are the changes
+# git add .
+# git commit -m "Your message"
+#copy paste git code from github
